b9b0f0c277eb37245eef428d6100fbbfd5866fb6
radCode <radekgnych1@gmail.com>
2026-01-06 17:07:17 +0100

    first function in js and  first Pokemons from API

--- script.js
+++ script.js
@@ -1,25 +1,88 @@
-
-let BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
-let currentPkCards = [];
-let pkCard = document.getElementById("pkCards");
+let BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=40&offset=0";
+let currentPokemons = [];
+let pkDetails = [];
+let currentUrl = 0;
+let pkThumbnail = document.getElementById("thumbnail");
+let pkDialog = document.getElementById("dialog");
+pkDialog.innerHTML = fullDialogHtml();
 
 function init() {
-    loadPokemonCards();
+    loadPkData();
+}
+
+function fullDialogHtml() {
+    return dialogHtmlHeaderContendTpl() + dialogHtmlSectionContentTpl() + dialogHtmlFooterContentTpl();
 }
 
 // Holt Daten asynchron vom Server
-async function loadPokemonCards() {
-    let response = await fetch(BASE_URL);           // Anfrage senden
-    let loadedPkCards = await response.json();     // Antwort in JSON umwandeln
-    
-    currentPkCards = loadedPkCards.results;      // Liste der Pokémon speichern
-       
-    for (let index = 0; index < currentPkCards.length; index++) {
-        pkCard.innerHTML += renderPokemonCards(index); 
+async function loadPkData() {
+    let response = await fetch(BASE_URL, { method: "GET" });        // Anfrage senden mit standart mässing mit  GET
+    let data = await response.json();                            // Antwort in JSON umwandeln
+    currentPokemons = data.results;                                     // Liste der Pokémon speichern
+    console.log(currentPokemons);
+
+    for (let i = 0; i < currentPokemons.length; i++) {
+        let detailResponse = await fetch(currentPokemons[i].url);
+        let detaildata = await detailResponse.json();
+        console.log(detaildata);
+
+
+        currentPokemons[i] = detaildata;
+
+        pkThumbnail.innerHTML += renderPokemonCardsTpl(i);
     }
 }
 
 
-function renderPokemonCards(index) {
-        return `<div class="thumbnails">${currentPkCards[index].name}</div>`;
+function renderPokemonCardsTpl(i) {
+    return `<div>
+                <button class="singel-thumbnail" type="button"  onclick="showPkDialog(this, ${[i]})">
+                       <span>${currentPokemons[i].name}</span>
+                           <img  class="thumbnail-images" 
+                           src="${currentPokemons[i].sprites.other.showdown.front_default}" 
+                        alt="Image${currentPokemons[i].name}">
+                 </button>
+           </div>`;
+}
+
+
+function showPkDialog(currentThumbnail, i) {
+
+    // let thumbnailContent = currentThumbnail;
+    //     thumbnailContent. 
+
+    pkDialog.showModal();
+
+    dialogHtmlHeaderContendTpl(currentThumbnail, i)
+}
+
+function closeDialog() {
+    pkDialog.close();
+
+}
+
+//function closeDialogOutsite(event) {
+//event.stopPropagation();
+//}
+
+function dialogHtmlHeaderContendTpl() {
+    return `<header id="headline" class="dialog-header" onclick="closeDialogOutsite(event)" >
+    <h2 id="imgTitle"></h2>
+    <button class="close-button" type="button" onclick="buttonCloseDialog()">&times;</button>
+    </header>`;
+}
+
+
+function dialogHtmlSectionContentTpl() {
+    return `<section  onclick="closeDialogOutsite(event)"><img id="imgModal" class="lagre_img" src="" alt="" title=""></section>`;
+}
+
+
+function dialogHtmlFooterContentTpl() {
+    return `<footer class="arrow_footer" id="dialogFooter">
+    <div  id="arrowContainer"  class="arrow_Container" onclick="closeDialogOutsite(event)">
+        <button id="arrowLeft"  class="arrow_button" onclick="clickButtonPrevious()"  type="button">&larr;</button>
+        <button id="arrowRight" class="arrow_button" onclick="clickButtonForward()" type="button">&rarr;</button>
+    </div>
+</footer>`;
 }
